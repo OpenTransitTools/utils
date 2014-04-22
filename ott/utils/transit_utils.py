4f7f1ebc31e87a8eb006678c5b81313b47480b2e
Frank Purcell <fxpurcell@gmail.com>
2014-04-22 00:21:39 -0700

    refactor: add exception handling to report db errors / no data from query

--- ott/utils/transit_utils.py
+++ ott/utils/transit_utils.py
@@ -108,3 +108,30 @@ def has_fare(itinerary):
                 ret_val = True
                 break
     return ret_val
+
+
+def make_short_name(gtfsdb_route, def_name=None):
+    ''' fix up the short name...
+    '''
+    ret_val = def_name
+    try:
+        ret_val = gtfsdb_route.route_short_name
+        if ret_val is None or len(ret_val) < 1:
+            ret_val = gtfsdb_route.route_long_name
+    
+        # strip off 'Line' from last word, ala MAX Blue Line == MAX Blue
+        if ret_val and ret_val.endswith('Line'):
+            ret_val = " ".join(ret_val.split()[:-1])
+        # special fix for Portland Streetcar
+        if 'Portland Streetcar' in ret_val and gtfsdb_route.route_long_name and len(gtfsdb_route.route_long_name) > 0:
+            ret_val = gtfsdb_route.route_long_name.replace('Portland', '').strip()
+        # fix WES
+        if ret_val and ret_val.startswith('WES '):
+            ret_val = "WES"
+        # fix Portland Aerial Tram
+        if ret_val and ret_val == 'Portland Aerial Tram':
+            ret_val = "Tram"
+    except:
+        pass
+
+    return ret_val
