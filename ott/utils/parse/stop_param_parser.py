7aa979a5ffa3cebf179c1ca480023b280a7a9d38
Frank Purcell <fxpurcell@gmail.com>
2014-07-16 17:38:05 -0700

    add locID per the param names that ATIS uses

--- ott/utils/parse/stop_param_parser.py
+++ ott/utils/parse/stop_param_parser.py
@@ -2,7 +2,7 @@ from ott.utils import date_utils
 from .param_parser import ParamParser
 from .route_param_parser import ROUTE_IDS, DIR_IDS
 
-STOP_IDS = ['stop_id', 'stopID',  'stopId', 'stop', 'Loc', 'loc'] 
+STOP_IDS = ['stop_id', 'stopID',  'stopId', 'stop', 'Loc', 'locID'] 
 
 class StopParamParser(ParamParser):
 
@@ -11,6 +11,6 @@ class StopParamParser(ParamParser):
         #import pdb; pdb.set_trace()
         self.date_str = self._parse_date()
         self.date = date_utils.str_to_date(self.date_str)
-        self.stop_id = self.get_first_val(STOP_IDS + ['id', 'Id'])
+        self.stop_id = self.get_first_val(STOP_IDS + ['Id'])
         self.route_id = self.get_first_val(ROUTE_IDS) 
         self.direction_id = self.get_first_val(DIR_IDS)
