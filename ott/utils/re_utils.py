cd1b264ea8ee45ecca403dbcd76ef2a21311e8c6
Frank Purcell <fxpurcell@gmail.com>
2015-03-09 17:21:40 -0700

    add regex routine

--- ott/utils/re_utils.py
+++ ott/utils/re_utils.py
@@ -2,3 +2,14 @@ import re
 import logging
 log = logging.getLogger(__file__)
 
+def contains(regexp, str):
+    ''' does string have one or more instances of regexp
+    '''
+    ret_val = False
+    try:
+        a = re.findall(regexp, str.strip())
+        if a and len(a) > 0:
+            ret_val = True
+    except:
+        pass
+    return ret_val
