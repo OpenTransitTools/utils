428e1df4a9179a7310073bf4b1cf3b5ecf5e83ba
Frank Purcell <fxpurcell@gmail.com>
2016-04-24 01:03:15 -0700

    big refactor ... all that use this config code will need to change

--- ott/utils/config.py
+++ ott/utils/config.py
@@ -1,91 +1,87 @@
 from ConfigParser import SafeConfigParser
 import json
 import logging
-log = logging.getLogger(__file__)
 import logging.config
+log = logging.getLogger(__file__)
 
 
 SECTION='view'
 INI=['app.ini', 'client.ini', 'services.ini', 'view.ini', 'production.ini']
-parser = None
-found_ini = None
-
-
-def get_parser(ini=INI):
-    ''' make the config parser
-    '''
-    global parser
-    global found_ini
-
-    try:
-        if parser is None:
-            candidates = []
-            for i in ini:
-                # add the .ini file and ./config/.ini file to our candidate file list
-                candidates.append(i)
-                candidates.append('./config/' + i)
-
-            parser = SafeConfigParser()
-            found_ini = parser.read(candidates)
-    except Exception, e:
-        log.info("Couldn't find an acceptable ini file from {0}\n{1}".format(candidates, e))
-
-    return parser
-
-def config_logger(ini=INI):
-    try:
-        get_parser(ini)
-        logging.config.fileConfig(found_ini, disable_existing_loggers=False)
-    except Exception, e:
-        pass
-
-def get(id, def_val=None, section=SECTION):
-    ''' get config value
-    '''
-    ret_val = def_val
-    try:
-        if get_parser():
-            ret_val = get_parser().get(section, id)
-            if ret_val is None:
-                ret_val = def_val
-    except Exception, e:
-        log.info("Couldn't find '{0}' in config under section '{1}'\n{2}".format(id, section, e))
-
-    return ret_val
-
-def get_int(id, def_val=None, section=SECTION):
-    ''' get config value as int (or go with def_val)
-    '''
-    ret_val = def_val
-    try:
-        v = get(id, def_val, section)
-        if v:
-            ret_val = int(v)
-    except Exception, e:
-        log.info("Couldn't find int value '{0}' in config under section '{1}'\n{2}".format(id, section, e))
-
-    return ret_val
-
-def get_json(id, section=SECTION):
-    ret_val = None
-    str_val = None
-    try:
-        str_val = get(id, section=section)
-        ret_val = json.loads(str_val)
-    except Exception, e:
-        log.info("Problems marshaling '{0}' into a python (json) object ({0} = {1}) \n{2}".format(id, str_val, e))
-        ret_val = str_val
-    return ret_val
 
 
 class OttConfig(object):
     section = SECTION
+    ini = INI
+    found_ini = None
 
-    def __init__(section=SECTION):
-        self.section = section
+    def __init__(self, ini=None, section=None):
+        if ini:     self.ini = ini
+        if section: self.section = section
 
-    def get(self, id, def_val=None, section=None): return get(id, def_val, section or self.section)
-    def get_int(self, id, def_val=None, section=None): return get(id, def_val, section or self.section)
-    def get_json(self, id, section=None): return get(id, section or self.section)
+    @property
+    def parser(self):
+        ''' make the config parser
+        '''
+        candidates = []
+        for i in self.ini:
+            # add the .ini file and ./config/.ini file to our candidate file list
+            candidates.append(i)
+            candidates.append('./config/' + i)
+
+        scp = SafeConfigParser()
+        self.found_ini = scp.read(candidates)
+        return scp
+
+    def get(self, id, section=None, def_val=None):
+        ''' get config value
+        '''
+        ret_val = def_val
+        try:
+            if section is None: section = self.section
+            ret_val = self.parser.get(section, id)
+            if ret_val is None:
+               ret_val = def_val
+        except Exception, e:
+            log.info("Couldn't find '{0}' in config under section '{1}'\n{2}".format(id, section, e))
+        return ret_val
+
+    def get_int(self, id, section=None, def_val=None):
+        ''' get config value as int (or go with def_val)
+        '''
+        ret_val = self.get(id, section, def_val)
+        try:
+            if ret_val:
+                ret_val = int(ret_val)
+        except Exception, e:
+            log.info("Couldn't convert '{0}' value into an INT type under section '{1}'\n{2}".format(id, section, e))
+        return ret_val
+
+    def get_json(self, id, section=None):
+        ret_val = None
+        str_val = None
+        try:
+            str_val = self.get(id, section=section)
+            ret_val = json.loads(str_val)
+        except Exception, e:
+            log.info("Problems marshaling '{0}' into a python (json) object ({0} = {1}) \n{2}".format(id, str_val, e))
+            ret_val = str_val
+        return ret_val
+
+    @classmethod
+    def config_logger(cls, ini):
+        try:
+            logging.config.fileConfig(ini, disable_existing_loggers=False)
+        except Exception, e:
+            pass
+
+    @classmethod
+    def factory(clfs, argv=None, section=None):
+        #import pdb; pdb.set_trace()
+        ini = INI
+        if argv and 'ini' in argv:
+            ini = argv['ini']
+        cfg = OttConfig(ini, section)
+        return cfg
+
 
 
