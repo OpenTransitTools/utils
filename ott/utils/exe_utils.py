2aa6af17bde1b6260cf9cdfaf724f152551e160a
Frank Purcell <fxpurcell@gmail.com>
2016-05-09 17:59:08 -0700

    add documentation ... fork stuff.

--- ott/utils/exe_utils.py
+++ ott/utils/exe_utils.py
@@ -7,21 +7,24 @@ log = logging.getLogger(__file__)
 def run_java(cmd_line, fork=False, big_xmx="-Xmx4096m", small_xmx="-Xmx1536m", java_cmd="java"):
     ''' run java ... if we get an exception, try to run again with lower heap size
     '''
+    #import pdb; pdb.set_trace()
     try:
         java_cmd = "{} {} {}".format(java_cmd, big_xmx, cmd_line)
         run_cmd(java_cmd, fork)
     except Exception, e:
+        # NOTE: 'fork' won't get you to see an exception here (because you fork the exception into another process)
         log.info(e)
         java_cmd = "{} {} {}".format(java_cmd, small_xmx, cmd_line)
         run_cmd(java_cmd, fork)
+        pass
 
 def run_cmd(cmd_line, fork=False):
     ''' run_cmd("sleep 200") will block for 200 seconds
         run_cmd("sleep 200", True) will background
 
         NOTE: some other options for piping output...
-        devnull = open(os.devnull, 'wb')
-        subprocess.Popen(['nohup', 'sleep', '100'], stdout=devnull, stderr=devnull)
+          devnull = open(os.devnull, 'wb')
+          subprocess.Popen(['nohup', 'sleep', '100'], stdout=devnull, stderr=devnull)
     '''
     log.info(cmd_line)
     if fork:
