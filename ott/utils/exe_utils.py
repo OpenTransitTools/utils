7c55a1381be53fbcc7f333114e43044e80abc1da
Frank Purcell <fxpurcell@gmail.com>
2016-05-10 13:51:10 -0700

    add shell flag for java

--- ott/utils/exe_utils.py
+++ ott/utils/exe_utils.py
@@ -4,21 +4,23 @@ import logging
 log = logging.getLogger(__file__)
 
 
-def run_java(cmd_line, fork=False, big_xmx="-Xmx4096m", small_xmx="-Xmx1536m", java_cmd="java"):
+def run_java(cmd_line, fork=False, big_xmx="-Xmx4096m", small_xmx="-Xmx1536m", java_cmd="java", shell=True):
     ''' run java ... if we get an exception, try to run again with lower heap size
     '''
     #import pdb; pdb.set_trace()
     try:
+        if big_xmx is None:
+            big_xmx = "-Xmx4096m"
         java_cmd = "{} {} {}".format(java_cmd, big_xmx, cmd_line)
-        run_cmd(java_cmd, fork)
+        run_cmd(java_cmd, fork, shell)
     except Exception, e:
         # NOTE: 'fork' won't get you to see an exception here (because you fork the exception into another process)
         log.info(e)
         java_cmd = "{} {} {}".format(java_cmd, small_xmx, cmd_line)
-        run_cmd(java_cmd, fork)
+        run_cmd(java_cmd, fork, shell)
         pass
 
-def run_cmd(cmd_line, fork=False):
+def run_cmd(cmd_line, fork=False, shell=True):
     ''' run_cmd("sleep 200") will block for 200 seconds
         run_cmd("sleep 200", True) will background
 
@@ -28,7 +30,7 @@ def run_cmd(cmd_line, fork=False):
     '''
     log.info(cmd_line)
     if fork:
-        subprocess.Popen(cmd_line.split())
+        subprocess.Popen(cmd_line.split(), shell=shell)
     else:
         os.system(cmd_line)
 
