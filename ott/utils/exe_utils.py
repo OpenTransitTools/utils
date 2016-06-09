96d0910ff9a71f4804fbf92040b28fc85827f896
Frank Purcell <fxpurcell@gmail.com>
2016-06-08 18:29:27 -0700

    more fork crap ... frustrating

--- ott/utils/exe_utils.py
+++ ott/utils/exe_utils.py
@@ -6,9 +6,9 @@ import logging
 log = logging.getLogger(__file__)
 
 
-def run_java(cmd_line, fork=False, big_xmx="-Xmx4096m", small_xmx="-Xmx1536m", java_cmd="java", shell=None, pid_file="pid.txt"):
+def run_java(cmd_line, fork=False, big_xmx="-Xmx4096m", small_xmx="-Xmx1536m", java_cmd="java", shell=True, pid_file="pid.txt"):
     ''' run java ... if we get an exception, try to run again with lower heap size
-        NOTE: shell is None here by default, so we can detect whether java needs a shell to operate (ala maps servers)
+        NOTE: shell is True, since we need a specific version of java ... this might make the whole pid file not be able to kill things
     '''
     if shell is None:
         shell = does_java_need_a_shell(java_cmd, fork)
@@ -24,12 +24,12 @@ def run_java(cmd_line, fork=False, big_xmx="-Xmx4096m", small_xmx="-Xmx1536m", j
         cmd_line = "{} {} {}".format(java_cmd, small_xmx, cmd_line)
         run_cmd(cmd_line, fork, shell, pid_file)
 
-
 def does_java_need_a_shell(java_cmd, fork):
     ''' does java cmd need a shell to run properly? ...
         if we get thrown an exception, assume that we do need a shell to execute java
     '''
     ret_val = False
+    java_cmd = "{} -version".format(java_cmd)
     try:
         if fork:
             process = subprocess.Popen(java_cmd, shell=False)
@@ -38,7 +38,6 @@ def does_java_need_a_shell(java_cmd, fork):
     except:
         ret_val = True
     return ret_val
-
 
 def run_cmd(cmd_line, fork=False, shell=False, pid_file="pid.txt"):
     ''' run_cmd("sleep 200") will block for 200 seconds
@@ -62,7 +61,6 @@ def run_cmd(cmd_line, fork=False, shell=False, pid_file="pid.txt"):
         write_pid_file(pid_file, pid)
     return pid
 
-
 def kill_old_pid(pid_file):
     ''' read pid file and then kill process
     '''
@@ -75,7 +73,6 @@ def kill_old_pid(pid_file):
     except Exception, e:
         log.debug(e)
 
-
 def write_pid_file(pid_file, pid):
     ''' write a pid file
     '''
@@ -83,7 +80,6 @@ def write_pid_file(pid_file, pid):
     pf.write(str(pid))
     pf.flush()
     pf.close()
-
 
 def kill(pid):
     ''' kill a process
