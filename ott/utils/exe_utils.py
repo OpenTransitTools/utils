043a039fe4fb71535626e2c9842fc68c44431fae
Frank Purcell <fxpurcell@gmail.com>
2016-06-08 17:40:37 -0700

    more pid stuff ... shell checking

--- ott/utils/exe_utils.py
+++ ott/utils/exe_utils.py
@@ -6,20 +6,35 @@ import logging
 log = logging.getLogger(__file__)
 
 
-def run_java(cmd_line, fork=False, big_xmx="-Xmx4096m", small_xmx="-Xmx1536m", java_cmd="java", shell=False, pid_file="pid.txt"):
+def run_java(cmd_line, fork=False, big_xmx="-Xmx4096m", small_xmx="-Xmx1536m", java_cmd="java", shell=None, pid_file="pid.txt"):
     ''' run java ... if we get an exception, try to run again with lower heap size
+        NOTE: shell is None here by default, so we can detect whether java needs a shell to operate (ala maps servers)
     '''
+    if shell is None:
+        shell = does_java_need_a_shell(java_cmd)
     try:
         if big_xmx is None:
             big_xmx = "-Xmx4096m"
-        java_cmd = "{} {} {}".format(java_cmd, big_xmx, cmd_line)
-        run_cmd(java_cmd, fork, shell, pid_file)
+        cmd_line = "{} {} {}".format(java_cmd, big_xmx, cmd_line)
+        run_cmd(cmd_line, fork, shell, pid_file)
     except Exception, e:
         # try again with smaller java heap memory request
         # NOTE: 'fork' won't get you to see an exception here (because you fork the exception into another process)
         log.info(e)
-        java_cmd = "{} {} {}".format(java_cmd, small_xmx, cmd_line)
-        run_cmd(java_cmd, fork, shell, pid_file)
+        cmd_line = "{} {} {}".format(java_cmd, small_xmx, cmd_line)
+        run_cmd(cmd_line, fork, shell, pid_file)
+
+
+def does_java_need_a_shell(java_cmd):
+    ''' does java cmd need a shell to run properly? ...
+        if we get thrown an exception, assume that we do need a shell to execute java
+    '''
+    ret_val = False
+    try:
+        subprocess.Popen(java_cmd, shell=False)
+    except:
+        ret_val = True
+    return ret_val
 
 
 def run_cmd(cmd_line, fork=False, shell=False, pid_file="pid.txt"):
@@ -44,6 +59,7 @@ def run_cmd(cmd_line, fork=False, shell=False, pid_file="pid.txt"):
         write_pid_file(pid_file, pid)
     return pid
 
+
 def kill_old_pid(pid_file):
     ''' read pid file and then kill process
     '''
@@ -56,6 +72,7 @@ def kill_old_pid(pid_file):
     except Exception, e:
         log.debug(e)
 
+
 def write_pid_file(pid_file, pid):
     ''' write a pid file
     '''
@@ -63,6 +80,7 @@ def write_pid_file(pid_file, pid):
     pf.write(str(pid))
     pf.flush()
     pf.close()
+
 
 def kill(pid):
     ''' kill a process
