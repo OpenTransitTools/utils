8082a2f8769599e09612c19afbe50e51099a7fca
Frank Purcell <fxpurcell@gmail.com>
2016-07-06 13:58:32 -0700

    camel meet snake, snake camel

--- ott/utils/string_utils.py
+++ ott/utils/string_utils.py
@@ -1,4 +1,5 @@
 import sys
+import string
 import logging
 log = logging.getLogger(__file__)
 
@@ -19,3 +20,17 @@ def is_in_string(in_str, targets):
                 break
 
     return ret_val
+
+def camel_to_underscore(name):
+    '''convert a camel case string to snake case'''
+    for char in string.ascii_uppercase:
+        name = name.replace(char, '_{0}'.format(char))
+    return name.lower()
+
+def underscore_to_camel(name):
+    '''convert a snake case string to camel case'''
+    string_list = name.strip('_').split('_')
+    result = string_list[0].lower()
+    for s in string_list[1:]:
+        result = '{0}{1}'.format(result, s.title())
+    return result
