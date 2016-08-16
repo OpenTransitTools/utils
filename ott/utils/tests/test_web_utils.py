6d2f7ce093196b68cb0f9eee8b37fe5da069b148
Frank Purcell <fxpurcell@gmail.com>
2016-08-16 12:40:12 -0700

    clean up test naming

--- ott/utils/tests/test_web_utils.py
+++ ott/utils/tests/test_web_utils.py
@@ -11,7 +11,7 @@ from ott.utils import web_utils
 SOLR_URL = "http://maps7.trimet.org:10880/solr/core/update"
 THIS_DIR = os.path.dirname(__file__)
 
-class TestSolr(unittest.TestCase):
+class TestWebUtils(unittest.TestCase):
 
     def setUp(self):
         pass
@@ -19,20 +19,19 @@ class TestSolr(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_add_data(self):
-        ''' test adding a document to a SOLR index
-            @TODO: query SOLR and test whether the
+    def test_http_post(self):
+        ''' test adding a document to a SOLR index with web_utils.post_file and post_data routines
+            @TODO: query SOLR and test whether the data is both added and deleted
         '''
         #import pdb; pdb.set_trace()
+
+        # add and comment data
         status = web_utils.post_file(SOLR_URL, os.path.join(THIS_DIR, "add.xml"))
         self.assertTrue(status == 200)
         status = web_utils.post_data(SOLR_URL, "<commit/>")
         self.assertTrue(status == 200)
 
-    def test_delete_data(self):
-        '''
-        '''
-        #import pdb; pdb.set_trace()
+        # delete this data
         status = web_utils.post_file(SOLR_URL, os.path.join(THIS_DIR, "del.xml"))
         self.assertTrue(status == 200)
         status = web_utils.post_data(SOLR_URL, "<commit/>")
