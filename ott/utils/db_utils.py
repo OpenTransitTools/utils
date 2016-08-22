2c96b51267c08ca33e450c035f1641e4f94cc14b
Frank Purcell <fxpurcell@gmail.com>
2016-08-22 16:39:21 -0700

    add pg database routines to check and create a new postgis db

--- ott/utils/db_utils.py
+++ ott/utils/db_utils.py
@@ -3,6 +3,19 @@ import logging
 log = logging.getLogger(__file__)
 
 
+
+def check_create_db(db_url, db_user, is_geospatial=False):
+    ''' generic check a database ... and create it if it doesn't exist
+        @TODO: add other database supports beyond postgres
+    '''
+    ret_val = True
+    if 'postgres' in db_url:
+        db_name = postgres_db_name_from_url(db_url)
+        ret_val = postgres_check_create_db(db_name, db_user, is_geospatial)
+
+    return ret_val
+
+
 def db_args():
     ''' create a generic database commandline arg PARSER '''
     import argparse
@@ -47,23 +60,6 @@ def db_gtfs_rt():
     return ses,eng
 
 
-def add_math_to_sqllite(conn, conn_record):
-    ''' This method is called for each new SQLAlchemy database connection. I'm using it as a connection decorator to
-        add math routines to a sqllite database
-
-        @note: check out the call to (above): event.listen(self.engine, 'connect', Database.connection)
-        @see:  http://docs.sqlalchemy.org/en/rel_0_8/core/events.html#sqlalchemy.events.PoolEvents
-    '''
-    if 'sqlite' in type(conn).__module__:        
-        print 'in method that adds math to sqllite'
-        conn.create_function("sin",     1, math.sin)
-        conn.create_function("cos",     1, math.cos)
-        conn.create_function("acos",    1, math.acos)
-        conn.create_function("sqrt",    1, math.sqrt)
-        conn.create_function("pow",     2, math.pow)
-        conn.create_function("radians", 1, math.radians)
-
-
 def decorate_engine(engine):
     from sqlalchemy import event
     event.listen(engine, 'connect', add_math_to_sqllite)
@@ -79,6 +75,23 @@ def closest_stops_tuple_to_dict(tup):
         'dist_miles' : tup[4]
     }
     return ret_val
+
+
+def add_math_to_sqllite(conn, conn_record):
+    ''' This method is called for each new SQLAlchemy database connection. I'm using it as a connection decorator to
+        add math routines to a sqllite database
+
+        @note: check out the call to (above): event.listen(self.engine, 'connect', Database.connection)
+        @see:  http://docs.sqlalchemy.org/en/rel_0_8/core/events.html#sqlalchemy.events.PoolEvents
+    '''
+    if 'sqlite' in type(conn).__module__:        
+        print 'in method that adds math to sqllite'
+        conn.create_function("sin",     1, math.sin)
+        conn.create_function("cos",     1, math.cos)
+        conn.create_function("acos",    1, math.acos)
+        conn.create_function("sqrt",    1, math.sqrt)
+        conn.create_function("pow",     2, math.pow)
+        conn.create_function("radians", 1, math.radians)
 
 
 def sqlite_nearest_query(schema=None, table='stops'):
@@ -121,3 +134,110 @@ def sqlite_query_nearest_stops(conn, lon, lat, count, schema, table):
     ret_val = conn.execute(clause, {'lon':lon, 'lat':lat}).fetchmany(count)
     return ret_val
 
+
+def postgres_check_db(db_name, db_user, db_table=None):
+    ret_val = True
+    con = None
+    try:
+        from psycopg2 import connect
+
+        # step 1: check the database connection
+        con = connect(dbname=db_user, database=db_name)
+
+        # step 2: (optionally) check a value from a given database
+        if db_table:
+            cur = con.cursor()
+            cur.execute('SELECT 1 from {}'.format(db_table))
+            item = cur.fetchone()
+            log.debug("POSTGRES item {} from table {} of db {}".format(item, db_table, db_name))
+    except Exception, e:
+        log.error("POSTGRES DATABASE ERROR : {}".format(e))
+        ret_val = False
+    finally:
+        if con:
+            con.close()
+    return ret_val
+
+
+def postgres_add_postgis(db_name, db_user):
+    ''' add postgis extension on a named database
+    '''
+    ret_val = True
+    con = None
+    try:
+        from psycopg2 import connect
+        from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+        con = connect(user=db_user, database=db_name)
+        cur = con.cursor()
+        cur.execute('CREATE EXTENSION postgis')
+        cur.close()
+        con.close()
+        con = None
+    except Exception, e:
+        log.error("POSTGRES DATABASE ERROR : {}".format(e))
+        ret_val = False
+    finally:
+        if con:
+            con.close()
+    return ret_val
+
+
+def postgres_create_db(db_name, db_user):
+    ''' create a postgres db...and also maybe add the postgis extension
+    '''
+    ret_val = True
+    con = None
+    try:
+        from psycopg2 import connect
+        from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+        # step 1: connect to global postgres database and then create new db
+        con = connect(user=db_user, database='postgres')
+        cur = con.cursor()
+        cur.execute('CREATE DATABASE {}'.format(db_name))
+        cur.close()
+        con.close()
+        con = None
+    except Exception, e:
+        log.error("POSTGRES DATABASE ERROR : {}".format(e))
+        ret_val = False
+    finally:
+        if con:
+            con.close()
+    return ret_val
+
+
+def postgres_db_name_from_url(db_url, def_val=None):
+    ret_val = def_val
+    try:
+        # assume db name is the last section of the db url, ala postgresql+psycopg2://127.0.0.1:5432/ott
+        ret_val = db_url.rsplit('/', 1)[1]
+    except Exception, e:
+        log.error("DB URL {} PARSE ERROR : {}".format(db_url, e))
+    return ret_val
+
+
+def postgres_check_create_db(db_name, db_user, is_geospatial=False):
+    '''
+    '''
+    import pdb; pdb.set_trace()
+    ret_val = True
+    try:
+        exists = postgres_check_db(db_name, db_user)
+        if exists:
+            ret_val = postgres_create_db(db_name, db_user)
+            if ret_val and is_geospatial:
+                ret_val = postgres_add_postgis(db_user, db_user)
+    except Exception, e:
+        log.error("POSTGRES CREATE DATABASE ERROR : {}".format(e))
+        ret_val = False
+
+    return ret_val
+
+
+def postgres_add_shp_file(db_url, db_user, is_geospatial=False):
+    '''
+        @see http://geospatialpython.com/2016/08/pure-python-loading-shapefiles-into.html
+    '''
+    ret_val = True
