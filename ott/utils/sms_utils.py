52171be422dd071b274b894f126d09108728ec57
Frank Purcell <fxpurcell@gmail.com>
2017-11-02 00:33:34 -0700

    fix some typos , add a print stmt

--- ott/utils/sms_utils.py
+++ ott/utils/sms_utils.py
@@ -1,7 +1,7 @@
 """ SMS Utils
 """
 
-DEF_FROM_NUM = "+16888726748"  # ott.tra.nsit bogus number
+DEF_FROM_NUM = "+16888726748"  # ott.tra.nsit bogus number ... need to probably get a number from sms gateway provider
 
 
 def twillo_sms_send(account_id, auth_token, to_number, message, from_number=DEF_FROM_NUM):
@@ -10,6 +10,7 @@ def twillo_sms_send(account_id, auth_token, to_number, message, from_number=DEF_
     from twilio.rest import Client
 
     # the following line needs your Twilio Account SID and Auth Token
+    print "Acc #:{}, Tok:{}, From:{}, To:{}, Msg:{}".format(account_id, auth_token, from_number, to_number, message)
     client = Client(account_id, auth_token)
     client.messages.create(to=to_number,
                            from_=from_number,
@@ -22,8 +23,8 @@ def main():
     parser = argparse.ArgumentParser(prog='twillo-sms', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--account_id',  '-a', help='Twillo Account (e.g., )')
     parser.add_argument('--auth_token',  '-u', help='Twillo Auth Token (e.g., )')
-    parser.add_argument('--from_number', '-f', help='Number you are texting')
-    parser.add_argument('--to_number',   '-t', help='Number you are texting from', default=DEF_FROM_NUM)
+    parser.add_argument('--from_number', '-f', help='Number you are texting', default=DEF_FROM_NUM)
+    parser.add_argument('--to_number',   '-t', help='Number you are texting to')
     parser.add_argument('msg', help='message you want to send', nargs='?', default='Hello from OTT')
     args = parser.parse_args()
     twillo_sms_send(args.account_id, args.auth_token, args.to_number, args.msg, args.from_number)
