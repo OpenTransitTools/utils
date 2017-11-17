b4db6b5121958baf220d82ec466f7d940d4aefe9
Frank Purcell <fxpurcell@gmail.com>
2017-11-17 10:30:28 -0800

    call urls

--- ott/utils/parse/logs/call_urls.py
+++ ott/utils/parse/logs/call_urls.py
@@ -1,7 +1,8 @@
 """ call urls from a log file
 
   examples:
-    bin/log_grep_urls -f ../app.log
+    bin/log_grep_urls -f ../app.log -s "Return trip"
+    bin/log_grep_urls -f ../app.log -s "No transit times available"
 """
 from base import Base
 from ott.utils import test_utils
@@ -11,23 +12,26 @@ class CallUrls(Base):
     """ call urls from file.urls
     """
     url_file = None
+    search = None
     results = None
 
     def __init__(self, args):
         self.url_file = args.file_name
+        self.search = args.search_term
 
     @classmethod
     def get_args(cls):
         import argparse
         parser = argparse.ArgumentParser(prog='call-urls', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
         parser.add_argument('--file_name', '-f', help='Urls file', default="app.urls")
+        parser.add_argument('--search_term', '-s', help='Optional search term (http response contains this)', default=None)
         args = parser.parse_args()
         return args
 
     def process(self):
         """ """
-        #import pdb; pdb.set_trace()
-        self.results = test_utils.loop_urls(self.url_file)
+        # import pdb; pdb.set_trace()
+        self.results = test_utils.loop_urls(self.url_file, match=self.search)
 
     def do_print(self):
         print self.results
--- ott/utils/test_utils.py
+++ ott/utils/test_utils.py
@@ -7,32 +7,34 @@ def loop_urls(url_file, match=None, batch_size=5, pause=None):
     """ run thru URLs, calling those urls in parallel batch sizes (stress and speed of processing)
     """
     t = m = f = 0
-    batch = []
 
     import web_utils
-    def process_batch():
+    def process_batch(url, t, m, f):
         """ process batch of urls
             TODO threads .... how to make multiple calls in parallel???
         """
-        for url in batch:
-            s, r = web_utils.get(url)
-            t += 1
-            if not s:
-                f += 1
-            if match:
-                if match in r:
-                    m += 1
-            else:
+        # import pdb; pdb.set_trace()
+        s, r = web_utils.get(url)
+        t += 1
+        if not s:
+            f += 1
+        if match:
+            if match in r:
                 m += 1
+        else:
+            m += 1
+        return t, m, f
 
     with open(url_file) as file:
         k = 0
+        batch = []
         for u in file:
             batch.append(u)
             k += 1
             if k == batch_size:
                 k = 0
-                process_batch
+                for url in batch:
+                    t, m, f = process_batch(url, t, m, f)
                 del batch[:]  # clear batch list
 
     ret_val = {'total': t, 'match': m, 'failed_connect': f}
