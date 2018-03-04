e7e565f88cdab20b7551413ef3799ea1ae65c573
Frank Purcell <fxpurcell@gmail.com>
2018-03-04 15:06:36 -0800

    work on the intersection code st_intersects

--- ott/utils/geo_db_utils.py
+++ ott/utils/geo_db_utils.py
@@ -20,23 +20,32 @@ try:
         name = 'ST_Collect'
         type = Geometry
 
-
-    def does_point_intersect_geom(point, geom, buffer=0.0):
-        """
-        return true or false whether point is in / out of the geom
-        """
-        log.debug('does point intersect this geom')
-        ret_val = False
-        return ret_val
-
-
-    def point_to_geom_distance(point, geom):
-        """
-        return true or false whether point is in / out of the geom
-        """
-        log.debug("distance between point and a geom (assuming they don't intersect")
-        ret_val = False
-        return ret_val
-
 except Exception as e:
     log.info(e)
+
+
+def does_point_intersect_geom(session, point, geom, buffer=0.0):
+    """
+    return true or false whether point is in / out of the geom
+    """
+    log.debug('does point intersect this geom')
+    ret_val = False
+    try:
+        ret_val = session.scalar(geom.ST_Intersects(point))
+    except Exception as e:
+        log.warn(e)
+
+    return ret_val
+
+
+def point_to_geom_distance(session, point, geom):
+    """
+    return true or false whether point is in / out of the geom
+    """
+    log.debug("distance between point and a geom (assuming they don't intersect")
+    ret_val = False
+    try:
+        ret_val = session.scalar(geom.ST_Intersects(point))
+    except Exception as e:
+        log.warn(e)
+    return ret_val
