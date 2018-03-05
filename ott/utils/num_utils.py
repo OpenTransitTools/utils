0a6cd02065fad2a0e03512f78d9af1e9e916c5cb
Frank Purcell <fxpurcell@gmail.com>
2018-03-05 09:59:14 -0800

    add some num conversions

--- ott/utils/num_utils.py
+++ ott/utils/num_utils.py
@@ -52,3 +52,56 @@ def array_item_to_int(list, index, def_val=None):
     except:
         pass
     return ret_val
+
+
+def meters_to_feet(meters):
+    return meters * 0.30480
+
+
+def feet_to_meters(feet, inches=0):
+    tot_inches = feet * 12 + inches
+    meters = tot_inches * 0.0254
+    return meters
+
+
+def feet_to_inches(feet, do_round=False):
+    inches = feet * 12
+    if do_round:
+        inches = math.round(inches)
+    return inches
+
+
+def inches_to_feet_inches(inches, include_units=True):
+    inches = math.round(inches)
+    feet = inches / 12
+    inches = inches % 12
+    ret_val = {'feet': feet, 'inches': inches}
+
+    if include_units:
+        ret_val['funits'] = 'foot' if feet == 1 or feet == -1 else 'feet'
+        ret_val['iunits'] = 'inch' if inches == 1 or inches == -1 else 'inches'
+
+    return ret_val
+
+
+def feet_to_feet_inches(feet, include_units=True):
+    inches = feet * 12
+    return inches_to_feet_inches(inches, include_units)
+
+
+def degrees_to_meters(degrees):
+    """
+    111195 = (Earth mean radius)*PI/180
+    (supposedly 'maximum error using this method is ~ 0.1%')
+    :see: https://stackoverflow.com/questions/12204834/get-distance-in-meters-instead-of-degrees-in-spatialite
+    """
+    ret_val = 111195 * degrees
+    return ret_val
+
+
+def degrees_to_feet(degrees):
+    meters = degrees_to_meters(degrees)
+    feet = num_utils.meters_to_feet(meters)
+    ret_val = feet_to_feet_inches(feet)
+    return ret_val
+
