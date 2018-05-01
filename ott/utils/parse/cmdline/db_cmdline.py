dbbf2fa58f4ccc53ab7e7afc8ea9d7f6a2a8e5b2
Frank Purcell <fxpurcell@gmail.com>
2018-05-01 12:24:27 -0700

    refactor cmdline fun

--- ott/utils/parse/cmdline/db_cmdline.py
+++ ott/utils/parse/cmdline/db_cmdline.py
@@ -24,13 +24,6 @@ def db_parser(prog_name='bin/load_db', tables=['Could be (Decarative) Base.metad
         help="database schema"
     )
     parser.add_argument(
-        '--is_geospatial',
-        '-geo',
-        '-g',
-        action="store_true",
-        help="add geometry columns"
-    )
-    parser.add_argument(
         '--create',
         '-create',
         '-c',
@@ -49,73 +42,15 @@ def db_parser(prog_name='bin/load_db', tables=['Could be (Decarative) Base.metad
         choices=tables, default=None, nargs='*',
         help="Limited list of TABLES to load, if blank, load all tables"
     )
+    is_spatial(parser)
     return parser
 
 
-def agency_option(parser, required=False, def_val='agency_id', help_msg="GTFS agency id (i.e., might also be a db schema name)"):
+def is_spatial(parser):
     parser.add_argument(
-        '--agency',
-        '-agency',
-        '-a',
-        required=required,
-        default=def_val,
-        help=help_msg
+        '--is_geospatial',
+        '-geo',
+        '-g',
+        action="store_true",
+        help="add geometry columns"
     )
-
-
-def api_key(parser, required=False, help_msg=None):
-    if help_msg is None:
-        help_msg = "api key needed to access this data"
-
-    parser.add_argument(
-        '--api_key',
-        '-akey',
-        '-ak',
-        required=required,
-        help=help_msg
-    )
-
-
-def gtfs_parser():
-    """ create a generic database commandline arg PARSER """
-    parser = db_parser('bin/load_gtfs')
-    agency_option(parser, True)
-    parser.add_argument(
-        '--url',
-        '-url',
-        '-u',
-        required=True,
-        help="url to gtfs feed (ala https://developers.google.com/transit/gtfs/examples/sample-feed.zip)"
-    )
-    return parser
-
-
-def gtfs_rt_parser(api_key_required=False, api_key_msg=None):
-    """ create a generic database commandline arg PARSER """
-    parser = db_parser('bin/load_gtfs_rt')
-    agency_option(parser, True)
-    if api_key_required:
-        api_key(parser, api_key_required, api_key_msg)
-    parser.add_argument(
-        '--alerts_url',
-        '-aurl',
-        '-al',
-        required=False,
-        help="url to gtfs-realtime *alerts* data feed"
-    )
-    parser.add_argument(
-        '--trips_url',
-        '-turl',
-        '-t',
-        required=False,
-        help="url to gtfs-realtime *trip updates* data feed"
-    )
-    parser.add_argument(
-        '--vehicles_url',
-        '-vurl',
-        '-v',
-        required=False,
-        help="url to gtfs-realtime *vehicle positions* data feed"
-    )
-
-    return parser
---
+++ ott/utils/parse/cmdline/gtfs_cmdline.py
@@ -0,0 +1,85 @@
+import db_cmdline
+
+import logging
+log = logging.getLogger(__file__)
+
+
+def blank_parser(prog_name='bin/gtfs'):
+    """
+    create a generic database commandline arg PARSER
+    """
+    import argparse
+    parser = argparse.ArgumentParser(
+        prog=prog_name,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    return parser
+
+
+def agency_option(parser, required=False, def_val='agency_id', help_msg="GTFS agency id (i.e., might also be a db schema name)"):
+    parser.add_argument(
+        '--agency',
+        '-agency',
+        '-a',
+        required=required,
+        default=def_val,
+        help=help_msg
+    )
+
+
+def api_key(parser, required=False, help_msg=None):
+    if help_msg is None:
+        help_msg = "api key needed to access this data"
+
+    parser.add_argument(
+        '--api_key',
+        '-akey',
+        '-ak',
+        required=required,
+        help=help_msg
+    )
+
+
+def gtfs_parser():
+    """ create a generic database commandline arg PARSER """
+    parser = db_cmdline.db_parser('bin/load_gtfs')
+    agency_option(parser, True)
+    parser.add_argument(
+        '--url',
+        '-url',
+        '-u',
+        required=True,
+        help="url to gtfs feed (ala https://developers.google.com/transit/gtfs/examples/sample-feed.zip)"
+    )
+    return parser
+
+
+def gtfs_rt_parser(api_key_required=False, api_key_msg=None):
+    """ create a generic database commandline arg PARSER """
+    parser = db_cmdline.db_parser('bin/load_gtfs_rt')
+    agency_option(parser, True)
+    if api_key_required:
+        api_key(parser, api_key_required, api_key_msg)
+    parser.add_argument(
+        '--alerts_url',
+        '-aurl',
+        '-al',
+        required=False,
+        help="url to gtfs-realtime *alerts* data feed"
+    )
+    parser.add_argument(
+        '--trips_url',
+        '-turl',
+        '-t',
+        required=False,
+        help="url to gtfs-realtime *trip updates* data feed"
+    )
+    parser.add_argument(
+        '--vehicles_url',
+        '-vurl',
+        '-v',
+        required=False,
+        help="url to gtfs-realtime *vehicle positions* data feed"
+    )
+
+    return parser
