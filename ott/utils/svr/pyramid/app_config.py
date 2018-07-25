e0a873c16f6aac02b5f51866cd0a7e78e845fbd0
Frank Purcell <fxpurcell@gmail.com>
2018-07-24 18:00:22 -0700

    add AppConfig util and get it running (in otp_client_js)

--- ott/utils/svr/pyramid/app_config.py
+++ ott/utils/svr/pyramid/app_config.py
@@ -3,28 +3,28 @@ log = logging.getLogger(__file__)
 
 
 class AppConfig(object):
-
-    # cache time - affects how long varnish cache will hold a copy of the data
-    cache_long = 36000  # 10 hours
-    cache_short = 600   # 10 minutes
-
-    settings = None
+    """
+    this class contains settings and configs for a Pyramid web app
+    create this on the main() entry
+    see setup.py entry points + config/*.ini [app:main] ala pserve (e.g., bin/pserve config/development.ini)
+    """
     config = None
+    pyramid = None
     wsgi_app = None
 
-    def __init__(self, **settings):
-        self.settings = settings
+    def __init__(self, **config):
+        self.config = config
 
         # step 1: enable pyamid config
         from pyramid.config import Configurator
-        self.config = Configurator(settings=settings)
+        self.pyramid = Configurator(settings=config)
 
         # step 2: enable logging from settings
         try:
             # logging config for pserve / wsgi
-            if settings and 'logging_config_file' in settings:
+            if config and 'logging_config_file' in config:
                 from pyramid.paster import setup_logging
-                setup_logging(settings['logging_config_file'])
+                setup_logging(config['logging_config_file'])
         except Exception as e:
             log.warn(e)
 
@@ -33,11 +33,19 @@ class AppConfig(object):
         this function returns a Pyramid WSGI application.
         """
         if self.wsgi_app is None:
-            self.wsgi_app = self.config.make_wsgi_app()
+            self.wsgi_app = self.pyramid.make_wsgi_app()
         return self.wsgi_app
 
     def config_include_scan(self, clazz):
-        self.config.include(clazz.do_view_config)
-        self.config.scan(clazz.__name__)
+        # step 1: assume that view.set_app_config(AppConfig instance) exists so we can pass the config on to view
+        #         and warn us if that's not the case
+        try:
+            clazz.set_app_config(self)
+        except Exception as e:
+            log.info(e)
 
+        # step 2: also assume that this object / modules class has a do_view_config() method where views are created
+        self.pyramid.include(clazz.do_view_config)
 
+        # step 3: finally, scan this view class for attributes to config view endpoints
+        self.pyramid.scan(clazz.__name__)
---
+++ ott/utils/svr/pyramid/globals.py
@@ -0,0 +1,3 @@
+# cache time - affects how long varnish cache will hold a copy of the data
+CACHE_LONG = 36000  # 10 hours
+CACHE_SHORT = 600  # 10 minutes
