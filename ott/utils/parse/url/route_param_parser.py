2d6270da34dc557069b2a917a486546c1a764204
Frank Purcell <fxpurcell@gmail.com>
2018-07-27 02:52:43 -0700

    improve param parsers ... child factories, default vals, etc...

--- ott/utils/parse/url/route_param_parser.py
+++ ott/utils/parse/url/route_param_parser.py
@@ -7,9 +7,16 @@ DIR_IDS = ['dir', 'direction', 'direction_id', 'dir_id', 'directionID', 'dirID',
 
 
 class RouteParamParser(ParamParser):
-    def __init__(self, params):
-        super(RouteParamParser, self).__init__(params)
+    def __init__(self, request):
+        super(RouteParamParser, self).__init__(request)
         date_str = self._parse_date()
         self.date = date_utils.str_to_date(date_str)
         self.route_id = self.get_first_val_trim(ROUTE_IDS + ['id'])
         self.direction_id = self.get_first_val(DIR_IDS)
+
+    def get_route_id(self, def_val=None):
+        return self.route_id if self.route_id else def_val
+
+    @classmethod
+    def factory(cls, request):
+        return RouteParamParser(request)
--- ott/utils/parse/url/stop_param_parser.py
+++ ott/utils/parse/url/stop_param_parser.py
@@ -6,11 +6,20 @@ STOP_IDS = ['stop_id', 'stopID',  'stopId', 'stop', 'Loc', 'locID', 'loc1']
 
 class StopParamParser(ParamParser):
 
-    def __init__(self, params):
-        super(StopParamParser, self).__init__(params)
-        #import pdb; pdb.set_trace()
+    def __init__(self, request):
+        super(StopParamParser, self).__init__(request)
         self.date_str = self._parse_date()
         self.date = date_utils.str_to_date(self.date_str)
         self.stop_id = self.get_first_val_trim(STOP_IDS + ['id'])
         self.route_id = self.get_first_val_trim(ROUTE_IDS) 
         self.direction_id = self.get_first_val(DIR_IDS)
+
+    def get_stop_id(self, def_val=None):
+        return self.stop_id if self.stop_id else def_val
+
+    def get_route_id(self, def_val=None):
+        return self.route_id if self.route_id else def_val
+
+    @classmethod
+    def factory(cls, request):
+        return StopParamParser(request)
