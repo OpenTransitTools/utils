b50aa1cf6de9a2a5633000b4d61c1f6341ffdc80
Frank Purcell <fxpurcell@gmail.com>
2018-10-22 14:50:17 -0700

    initial add of ws response objects

---
+++ ott/utils/ws/geojson_base.py
@@ -0,0 +1,4 @@
+"""
+This is the default response object for OTT's geojson objects.
+It's a default blah blah blah...
+"""
---
+++ ott/utils/ws/otp_ti_base.py
@@ -0,0 +1,9 @@
+"""
+This is the default response object for OpenTripPlanner Transit Index (TI)
+It's defined here, to be shared between projects (otp_client_py and loader)
+"""
+def d():
+    'name': 'blah'
+    'url': 'https://trimet.org/transitcenters/index.htm'
+    'lat': 1.0
+    'lon': 1.0
