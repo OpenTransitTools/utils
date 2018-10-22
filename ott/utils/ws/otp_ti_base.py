492b1a536495996ff519e0a5c3694ec130cc47c5
Frank Purcell <fxpurcell@gmail.com>
2018-10-22 15:49:38 -0700

    refine TI base object

--- ott/utils/ws/otp_ti_base.py
+++ ott/utils/ws/otp_ti_base.py
@@ -1,9 +1,32 @@
-"""
-This is the default response object for OpenTripPlanner Transit Index (TI)
-It's defined here, to be shared between projects (otp_client_py and loader)
-"""
-def d():
-    'name': 'blah'
-    'url': 'https://trimet.org/transitcenters/index.htm'
-    'lat': 1.0
-    'lon': 1.0
+class OtpTiBase(object):
+    """
+    Base class for OpenTripPlanner (OTP) Transit Index (TI) web services
+
+    Park & Rides:
+        https://<domain & port>/otp/routers/default/park_and_ride
+
+    Routes and Routes serving a stop:
+        https://<domain & port>/otp/routers/default/index/routes
+        https://<domain & port>/otp/routers/default/index/stops/TriMet:5516/routes
+
+    Stops:
+        https://<domain & port>/otp/routers/default/index/stops?
+          minLat=45.50854243338104&maxLat=45.519789433696744&minLon=-122.6960849761963&maxLon=-122.65591621398927
+
+        https://<domain & port>/otp/routers/default/index/stops?
+          radius=1000&lat=45.4926336&lon=-122.63915519999999
+
+    Stop Schedules:
+        https://<domain & port>/otp/routers/default/index/stops/TriMet:823/stoptimes?timeRange=14400
+    """
+    def __init__(self, args={}):
+        self.set_attribute_via_dict('agencyName', args)
+        self.set_attribute_via_dict('id', args)
+        self.set_attribute_via_dict('name', args)
+        self.set_attribute_via_dict('lat', args)
+        self.set_attribute_via_dict('lon', args)
+        self.set_attribute_via_dict('url', args)
+
+    def set_attribute_via_dict(self, name, vals, always_cpy=False, def_val=None):
+        from .. import object_utils
+        object_utils.safe_set_from_dict(self, name, vals, always_cpy, def_val)
