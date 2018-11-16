47e9aaeca93c5cce68c7a4ded06fce3882be7d7a
Frank Purcell <fxpurcell@gmail.com>
2018-11-16 14:55:34 -0800

    chmod util

--- ott/utils/file_utils.py
+++ ott/utils/file_utils.py
@@ -114,6 +114,12 @@ def file_size(file_path):
     return s.st_size
 
 
+def chmod(file_path, perms=0o644):
+    """ py 2.x == 0755 vs py 3.x 0o755 """
+    # import pdb; pdb.set_trace()
+    os.chmod(file_path, perms)  # py 3.x ...
+
+
 def touch(file_path):
     try:
         os.utime(file_path, None)
