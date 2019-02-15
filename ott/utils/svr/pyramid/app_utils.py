30e2c74f7f23d9bac0f92a1bae13191a270e5a51
Frank Purcell <fxpurcell@gmail.com>
2019-02-15 10:04:44 -0800

    add logging ... see how much time a req/rep takes

--- ott/utils/svr/pyramid/app_utils.py
+++ ott/utils/svr/pyramid/app_utils.py
@@ -1,3 +1,7 @@
+import logging
+log = logging.getLogger(__file__)
+
+
 MAX_AGE = 1728000
 
 
@@ -5,6 +9,7 @@ def set_max_age(age):
     """:see 'Access-Control-Max-Age': str(MAX_AGE) below"""
     global MAX_AGE
     MAX_AGE = age
+    log.info("set Access-Control-Max-Age response header to {}".format(age))
 
 
 def add_cors_headers_response_callback(event):
@@ -26,6 +31,8 @@ def add_cors_headers_response_callback(event):
             'Access-Control-Allow-Credentials': 'true',
             'Access-Control-Max-Age': str(MAX_AGE),
         })
+        log.debug("CORS headers will be in the response")
 
     # set the function above to be called for each response, where we'll set the CORS headers
+    log.debug("set request /response event - CORS headers")
     event.request.add_response_callback(cors_headers)
