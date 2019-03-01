169f759290a2f2c45ee4bae39dd9832a8cd17f8a
Purcell <PurcellF@tri-met.org>
2019-03-01 00:41:09 +0000

    update file perms; also set def distance on txt planner to 3/4 mi to match other systems

--- ott/utils/html_utils.py
+++ ott/utils/html_utils.py
@@ -55,7 +55,7 @@ def planner_form_params(request):
     dt = date_utils.get_day_info()
     tm = date_utils.get_time_info()
 
-    walk_max_def = 1609
+    walk_max_def = 1260
     bike_max_def = 4828
     def_max_dist = walk_max_def
     def_optimize = "QUICK"
