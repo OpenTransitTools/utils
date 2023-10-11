ebff0dfec4408293db019c95d431f762923774fe
Frank Purcell <fxpurcell@gmail.com>
2023-10-11 15:47:09 -0700

    fix: add url and port to cmd line

--- ott/utils/parse/cmdline/osm_cmdline.py
+++ ott/utils/parse/cmdline/osm_cmdline.py
@@ -41,8 +41,17 @@ def osm_parser_args(prog_name, **kwargs):
     return osm_parser(prog_name, True, **kwargs)
 
 
-def geoserver_parser(prog_name='bin/generate_geoserver_config', do_parse=True, def_dir="data_dir"):
+def geoserver_parser(prog_name='bin/generate_geoserver_config', do_parse=True):
     """ create a generic GEOSERVER processor commandline arg PARSER """
+
+    # TODO: think about moving some of this to db_cmdline
+
+    # default values
+    def_dir = "data_dir"
+    workspace = "ott"
+    db_url = "localhost"
+    db_port = "5432"
+
     parser = blank_parser(prog_name)
     parser.add_argument(
         '--data_dir',
@@ -53,15 +62,32 @@ def geoserver_parser(prog_name='bin/generate_geoserver_config', do_parse=True, d
         help="path to geoserver 'data' directory"
     )
     parser.add_argument(
+        '--workspace',
+        '-workspace',
+        '-ws',
+        required=False,
+        default=workspace,
+        help="path to geoserver 'data' directory"
+    )
+    parser.add_argument(
         '--db_url',
         '-db_url',
         '-db',
+        '-url',
         required=False,
-        default="localhost",
+        default=db_url,
         help="db url (localhost or docker url, ala 'db')"
     )
+    parser.add_argument(
+        '--db_port',
+        '-db_port',
+        '-port',
+        required=False,
+        default=db_port,
+        help="db port (5432 is the default pg port)"
+    )
+
     ret_val = parser
     if do_parse:
         ret_val = parser.parse_args()
     return ret_val
-
